169d61aaca4aa245fc3297c2d4e4c5ff2dbcd1be
Dawid <74910684+chomiczdawid@users.noreply.github.com>
2023-01-14 22:35:31 +0100

    comments interface.py

--- interface.py
+++ interface.py
@@ -1,7 +1,8 @@
-#!pip install flask
+# !pip install flask
+# importing flask library and necessary modules
 from flask import Flask, render_template, request
 
-# Załadowanie listy z parametrami modelu
+# Loading model parameters from params.txt file generated in modeling.ipynb
 lparams = []
 with open('params.txt', 'r') as fp:
     for line in fp:
@@ -9,15 +10,18 @@ with open('params.txt', 'r') as fp:
         lparams.append(x)
 lparams = [float(i) for i in lparams]
 
-# Wygenerowanie strony głównej html za pomocą Flaska
+# Generating flask process
 app = Flask(__name__)
 
+# Defining main page route rendering the page on hosting server
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# Defining form subpage
 @app.route("/form/", methods=['POST'])
 def form():
+    # Assigning parameters from inputs
     area_size = request.form.get("input_area")
     s_rooms	= request.form.get("input_rooms")
     s_floor	= request.form.get("input_floor")
@@ -27,6 +31,8 @@ def form():
     furnished = request.form.get("input_furnished")
     s_levels = request.form.get("input_levels")
     
+    ## Converting input variables to binary lists corresponding with model parameters
+    # Rooms variable
     if s_rooms == '1':
         l_rooms = [1,0,0,0]
     elif s_rooms == '2':
@@ -35,7 +41,7 @@ def form():
         l_rooms = [0,0,1,0]
     elif s_rooms == '4+':
         l_rooms = [0,0,0,1]
-
+    # Floor variable
     if s_floor == '1':
         l_floor = [1,0,0,0,0,0,0]
     elif s_floor == '2':
@@ -50,7 +56,7 @@ def form():
         l_floor = [0,0,0,0,0,1,0]
     elif s_floor == '>10':
         l_floor = [0,0,0,0,0,0,1]
-
+    # Year variable
     if s_year == '1990-2005':
         l_year = [1,0,0,0,0]
     elif s_year == '2005-2015':
@@ -61,24 +67,24 @@ def form():
         l_year = [0,0,0,1,0]
     elif s_year == '>=2020':
         l_year = [0,0,0,0,1]
-
+    # Landlord type variable
     if s_landlord_type == 'osoba_prywatna':
         l_landlord_type = [0,1]
     elif s_landlord_type == 'firma':
         l_landlord_type = [1,0]
-
+    # Local type variable
     if s_local_type == 'inne':
         l_local_type = [1,0,0]
     elif s_local_type == 'komercyjne':
         l_local_type = [0,1,0]
     elif s_local_type == 'mieszkalne':
         l_local_type = [0,0,1]
-
+    # Furnished variable
     if furnished == 'nie':
         l_furnished = [1,0]
     elif furnished == 'tak':
         l_furnished = [0,1]
-
+    # Levels variable
     if s_levels == '3':
         l_levels = [1,0,0,0,0]
     elif s_levels == '4':
@@ -89,8 +95,11 @@ def form():
         l_levels = [0,0,0,1,0]
     elif s_levels == '>6':
         l_levels = [0,0,0,0,1]
-    
+    # Combining generated binary lists in order correpsonding with model parameters order in params.txt
     multipliers = [1] + [area_size] + l_rooms + l_floor + l_landlord_type + l_local_type + l_furnished + l_levels
+    # Multiplying converted input values by model parameters values from loaded params.txt file
     multiplied = [i1 * int(i2) for i1, i2 in zip(lparams, multipliers)]
+    # Adding up the multiplied values to get the final result, rounding output to two decimal places
     result = round(sum(multiplied), 2)
+    # Rendering result page from form.html and passing result variable to show on the web page
     return render_template("form.html", result=result)
